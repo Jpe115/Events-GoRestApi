798fb2a369160e32f1851a0800ab10ba31ed2f3e
Josue Palafox <jpalafoxe90@gmail.com>
2024-06-14 16:50:56 -0700

    first commit

---
+++ routes/routes.go
@@ -0,0 +1,22 @@
+package routes
+
+import (
+	"example.com/rest_api/middlewares"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(server *gin.Engine) {
+	server.GET("/events", getEvents)
+	server.GET("/events/yours", middlewares.Authenticate, getYourEvents)
+	server.GET("/events/:id", getEvent)
+
+	server.POST("/events", middlewares.Authenticate, createEvent)
+	server.PUT("/events/:id", middlewares.Authenticate, updateEvent)
+	server.DELETE("/events/:id", middlewares.Authenticate, deleteEvent)
+
+	server.POST("/events/:id/register", middlewares.Authenticate, registerForEvent)
+	server.DELETE("/events/:id/register", middlewares.Authenticate, cancelRegistration)
+
+	server.POST("/signup", signUp)
+	server.POST("/login", login)
+}
